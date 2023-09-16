d6e3c0e4a71cb1d81bd08f5f8a3ec4cb9969e0b2
CogRahul <2122119@cognizant.com>
2023-09-17 02:22:06 +0530

    Auto Changed Test-Output files

--- src/test/java/com/testrunner/OffersPageTest.java
+++ src/test/java/com/testrunner/OffersPageTest.java
@@ -12,7 +12,6 @@ import com.baseclass.BaseClass;
 public class OffersPageTest extends BaseClass {
 
 	@Test(dependsOnGroups = {"flight"}, groups="offer")
-//	@Test
 	public void OfferDetailsTest() throws IOException, InterruptedException {
 		HomePage hp = new HomePage(driver);
 		OffersPage ofp = hp.navigatingToOffersPage();
