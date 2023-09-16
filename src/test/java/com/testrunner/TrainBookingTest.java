ba697b32f1dc4b1ec6cd1798a35bf0d4f1ab631e
RahulChoudhary1201 <rk155898@gmail.com>
2023-09-17 02:07:25 +0530

    Implemented Trains Object Page, TrainTest and changes Xpath for day of calendar

---
+++ src/test/java/com/testrunner/TrainBookingTest.java
@@ -0,0 +1,24 @@
+package com.testrunner;
+
+import org.testng.annotations.Test;
+
+import com.Objects.HomePage;
+import com.Objects.TrainBooking;
+import com.baseclass.BaseClass;
+
+public class TrainBookingTest extends BaseClass{
+	
+//	@Test(dependsOnGroups = {"offer"}, groups = {"train"})
+	@Test
+	public void srcAnddestSettingTest() throws InterruptedException {
+		HomePage hp = new HomePage(driver);
+		TrainBooking tb= hp.navigateToTrains();
+		String title = tb.getTrainPageTitle();
+		System.out.println(title);
+		tb.setSrcAndDest("Pune", "Durg");
+		tb.setDate("22", "December", "2023");
+		tb.searchClick();
+		Thread.sleep(5000);
+	}
+
+}
